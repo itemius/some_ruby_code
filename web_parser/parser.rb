3e04db2311dd9d6971153e3962f001e3f8d5e90d
itemius <itemius@gmail.com>
2015-10-23 00:13:00 +0300

    redirect to https fixed

--- web_parser/parser.rb
+++ web_parser/parser.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open('http://en.wikipedia.org/wiki/HTML'))
+page = Nokogiri::HTML(open('https://en.wikipedia.org/wiki/HTML'))
 page.css('div#content div#bodyContent table.infobox tr th').each do |el|
    puts el.text
 end
